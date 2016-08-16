2e1d1001aab38ade6c19c922808d1ffb28e681cc
niehaixingchen <niehaixingchen@163.com>
2016-08-16 09:20:02 +0800

    init migu

--- proj.android/src/org/cocos2dx/lib/MiGuSdk.java
+++ proj.android/src/org/cocos2dx/lib/MiGuSdk.java
@@ -1,12 +1,12 @@
 package org.cocos2dx.lib;
 
-import com.umeng.analytics.game.UMGameAgent;
-
+import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.widget.Toast;
 import cn.cmgame.billing.api.BillingResult;
 import cn.cmgame.billing.api.GameInterface;
+
+import com.umeng.analytics.game.UMGameAgent;
 
 public class MiGuSdk implements ExternSdkInterface{
 	private static final int PAY_RESULT_SUCCESS = 0;
@@ -19,6 +19,7 @@ public class MiGuSdk implements ExternSdkInterface{
 	MiGuSdk(Context context) {
 		mContext = context;
 		initItemData();
+		GameInterface.initializeApp((Activity) mContext);
 	}
 	
 	class ItemData
@@ -60,53 +61,53 @@ public class MiGuSdk implements ExternSdkInterface{
 			items[i] = new ItemData(costs[i], names[i]);
 		}
 	}
+	
+	GameInterface.IPayCallback payCallback = new GameInterface.IPayCallback() {
+		@Override
+		public void onResult(int resultCode, String billingIndex, Object obj) {			
+			String result = "";
+			int payResult = PAY_RESULT_SUCCESS;
+			switch (resultCode) {
+			case BillingResult.SUCCESS:
+				if ((BillingResult.EXTRA_SENDSMS_TIMEOUT + "").equals(obj
+						.toString())) {
+					result = "短信计费超时";
+					payResult = PAY_RESULT_TIMEOUT;
+				} else {
+					result = "购买道具：[" + billingIndex + "] 成功！";
+					payResult = PAY_RESULT_SUCCESS;
+					int itemId = getItemIndex(billingIndex);
+					if(itemId > 0 && itemId <= items.length)
+					{
+						ItemData data = items[itemId - 1];
+						UMGameAgent.pay(data.cost ,data.name, 1, 1, 5);
+					}		
+				}
+				break;
+			case BillingResult.FAILED:
+				result = "购买道具：[" + billingIndex + "] 失败！";
+				payResult = PAY_RESULT_FAILED;
+				break;
+			default:
+				result = "购买道具：[" + billingIndex + "] 取消！";
+				payResult = PAY_RESULT_CANCEL;
+				break;
+			}
+			Toast.makeText(mContext, result, Toast.LENGTH_SHORT).show();
+			onPurchase(payResult);
+		}
+	};
 
 	//itemId 是从1开始
 	public void purchase(int itemId) {
 		// 计费结果的监听处理，合作方通常需要在收到SDK返回的onResult时，告知用户的购买结果
-		final GameInterface.IPayCallback payCallback = new GameInterface.IPayCallback() {
-			@Override
-			public void onResult(int resultCode, String billingIndex, Object obj) {			
-				String result = "";
-				int payResult = PAY_RESULT_SUCCESS;
-				switch (resultCode) {
-				case BillingResult.SUCCESS:
-					if ((BillingResult.EXTRA_SENDSMS_TIMEOUT + "").equals(obj
-							.toString())) {
-						result = "短信计费超时";
-						payResult = PAY_RESULT_TIMEOUT;
-					} else {
-						result = "购买道具：[" + billingIndex + "] 成功！";
-						payResult = PAY_RESULT_SUCCESS;
-						int itemId = getItemIndex(billingIndex);
-						if(itemId > 0 && itemId <= items.length)
-						{
-							ItemData data = items[itemId - 1];
-							UMGameAgent.pay(data.cost ,data.name, 1, 1, 5);
-						}		
-					}
-					break;
-				case BillingResult.FAILED:
-					result = "购买道具：[" + billingIndex + "] 失败！";
-					payResult = PAY_RESULT_FAILED;
-					break;
-				default:
-					result = "购买道具：[" + billingIndex + "] 取消！";
-					payResult = PAY_RESULT_CANCEL;
-					break;
-				}
-				Toast.makeText(mContext, result, Toast.LENGTH_SHORT).show();
-				onPurchase(payResult);
-			}
-		};
-
 		String billingIndex = getBillingIndex(itemId);
 		GameInterface.doBilling(mContext, true, true, billingIndex, null,
 				payCallback);
 	}
 
 	private String getBillingIndex(int i) {
-		if (i < 9) {
+		if (i < 10) {
 			return "00" + i;
 		} else {
 			return "0" + i;
